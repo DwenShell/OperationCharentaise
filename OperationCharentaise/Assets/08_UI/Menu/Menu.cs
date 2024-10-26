283a7a614d5731723a5921087a0606416ec68f57
Voliette <violette.chasseriau@ensam.eu>
2024-10-26 16:16:17 +0200

    StateMachine

--- OperationCharentaise/Assets/08_UI/Menu/Menu.cs
+++ OperationCharentaise/Assets/08_UI/Menu/Menu.cs
@@ -1,16 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+public enum State
+{
+    MENU,
+    VICTORY,
+    INSTRUCT,
+}
 public class Menu : MonoBehaviour
 {
+    public State state;
     public GameObject guiWin;
-    public string sceneName;
- 
+    public GameObject guiMenu;
+    public GameObject guiInstruct;
+
+    static public StateMachine instance;
+
+    
+
+    private void Start()
+    {
+        state=State.MENU;
+    }
+
+    private void Update()
+    {
+        guiMenu.SetActive(state==State.MENU);
+        guiWin.SetActive(state == State.VICTORY);
+        guiInstruct.SetActive(state==State.INSTRUCT);
+    }
+
+    public void SetState(State newState)
+    {
+        state=newState;
+    }
     public void OnClickPlay()
     {
-        SceneManager.LoadScene(sceneName);
+       SetState(State.INSTRUCT );
     }
 
     public void OnClickQuit()
